bc3757aaaf5f8d3d3901edddd2c05b23cc047058
Alex Espinoza <aespinoza@aspria.net>
2016-09-20 17:40:36 -0400

    Don't make price a required field for Show model

--- web/models/show.ex
+++ web/models/show.ex
@@ -4,7 +4,7 @@ defmodule UpcomingShowFinder.Show do
   schema "shows" do
     field :headliner, :string, null: false
     field :openers, :string
-    field :price, :string, null: false
+    field :price, :string
     field :date, :string, null: false
     belongs_to :source, UpcomingShowFinder.Source
 
@@ -17,6 +17,6 @@ defmodule UpcomingShowFinder.Show do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:headliner, :openers, :price, :date, :source_id])
-    |> validate_required([:headliner, :price, :date, :source_id])
+    |> validate_required([:headliner, :date, :source_id])
   end
 end
