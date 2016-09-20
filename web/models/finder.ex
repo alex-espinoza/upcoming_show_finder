1d60be2fbfb2fff4c4e82c0bc10f22b9da2d5452
Alex Espinoza <aespinoza@aspria.net>
2016-09-20 17:42:14 -0400

    Finish up Finder and RoyaleParser, maybe parse date string from site but might not because it seems to not be consistent and have many formats

--- web/models/finder.ex
+++ web/models/finder.ex
@@ -4,6 +4,7 @@ defmodule UpcomingShowFinder.Finder do
   import Ecto.Query
   alias UpcomingShowFinder.Repo
   alias UpcomingShowFinder.Source
+  alias UpcomingShowFinder.Show
 
   def prepare_to_scrape do
     Logger.info("Finder is preparing to scrape for new shows")
@@ -11,64 +12,74 @@ defmodule UpcomingShowFinder.Finder do
     Enum.each(sources, &scrape(&1))
   end
 
-  def scrape(source) do
+  defp scrape(source) do
     parser_module = Module.concat([UpcomingShowFinder, "#{source.parser}Parser"])
 
     source
     |> make_http_request
     |> parse_response_body_for_block_elements(parser_module)
-    |> parse_each_block_element(parser_module)
+    |> parse_each_block_element(parser_module, source.id)
   end
 
-  def make_http_request(source) do
+  defp make_http_request(source) do
     HTTPoison.get!(source.url)
   end
 
-  def parse_response_body_for_block_elements(response, parser_module) do
+  defp parse_response_body_for_block_elements(response, parser_module) do
     block_element_selector = apply(parser_module, :block_element_selector, [])
     Floki.find(response.body, block_element_selector)
   end
 
-  def parse_each_block_element(block_elements, parser_module) do
-    #IO.inspect(List.first(block_elements))
-    Enum.each(block_elements, &create_show_from_data_in_element(&1, parser_module))
+  defp parse_each_block_element(block_elements, parser_module, source_id) do
+    Enum.each(block_elements, &create_show_from_data_in_element(&1, parser_module, source_id))
   end
 
-  def create_show_from_data_in_element(block_element, parser_module) do
+  defp create_show_from_data_in_element(block_element, parser_module, source_id) do
+    parse_show_data_from_element(block_element, parser_module, source_id)
+    |> save_show
+  end
+
+  defp parse_show_data_from_element(block_element, parser_module, source_id) do
     headliner_selector = apply(parser_module, :headliner_selector, [])
     openers_selector = apply(parser_module, :openers_selector, [])
-    # price_selector = apply(parser_module, :price_selector, [])
-    # date_selector = apply(parser_module, :date_selector, [])
+    price_selector = apply(parser_module, :price_selector, [])
+    date_selector = apply(parser_module, :date_selector, [])
 
     headliner = Floki.find(block_element, headliner_selector)
-                |> Floki.text
-                |> String.trim
+                |> parse_headliner_elements(parser_module)
 
     openers = Floki.find(block_element, openers_selector)
               |> parse_openers_elements(parser_module)
 
-    IO.puts("---")
-    IO.puts(headliner)
-    IO.inspect(openers)
+    price = Floki.find(block_element, price_selector)
+            |> parse_price_elements(parser_module)
+
+    date = Floki.find(block_element, date_selector)
+           |> parse_date_elements(parser_module)
+
+    %{headliner: headliner, openers: openers, price: price, date: date, source_id: source_id}
   end
 
-  def parse_openers_elements(openers_elements, parser_module) do
+  defp save_show(show_data) do
+    changeset = Show.changeset(%Show{}, show_data)
+    IO.puts("---")
+    IO.inspect(changeset)
+    Repo.insert!(changeset)
+  end
+
+  defp parse_headliner_elements(headliner_elements, parser_module) do
+    apply(parser_module, :parse_headliner_elements, [headliner_elements])
+  end
+
+  defp parse_openers_elements(openers_elements, parser_module) do
     apply(parser_module, :parse_openers_elements, [openers_elements])
   end
+
+  defp parse_price_elements(price_elements, parser_module) do
+    apply(parser_module, :parse_price_elements, [price_elements])
+  end
+
+  defp parse_date_elements(date_elements, parser_module) do
+    apply(parser_module, :parse_date_elements, [date_elements])
+  end
 end
-
-
-# defmodule Scraper do
-#   def get_page_info do
-#     response = HTTPoison.get!("http://httparrot.herokuapp.com/get")
-#     {_, response} = JSON.decode(response.body)
-#     response
-#   end
-
-#   # https://varvy.com/pagespeed/wicked-fast.html
-#   def parse(url \\ "http://www.boweryboston.com/") do
-#     response = HTTPoison.get!(url)
-#     #Floki.find(response.body, "a")
-#     Floki.find(response.body, ".list-view-item")
-#   end
-# end
--- web/models/parsers/royale_parser.ex
+++ web/models/parsers/royale_parser.ex
@@ -5,20 +5,62 @@ defmodule UpcomingShowFinder.RoyaleParser do
 
   def openers_selector, do: ".tribe-events-event-details .tribe-events-meta-group .tribe-meta-value"
 
+  def price_selector, do: ".tribe-events-event-details .tribe-events-meta-group .tribe-meta-value"
+
+  def date_selector, do: ".tribe-events-event-details .tribe-events-event-meta .tribe-event-date-start"
+
+  def parse_headliner_elements(headliner_elements) do
+    headliner_elements
+    |> Floki.text
+    |> String.trim
+  end
+
   def parse_openers_elements(openers_elements) do
-    first_element  = List.first(openers_elements)
-                     |> Floki.text
-                     |> String.trim
-    is_dollar_amount = Regex.match?(~r/\$\d/, first_element)
+    first_element_text  = List.first(openers_elements)
+                          |> Floki.text
+                          |> String.trim
 
-    get_openers(first_element, is_dollar_amount)
+    is_dollar_amount = Regex.match?(~r/\$\d/, first_element_text)
+
+    get_openers(first_element_text, is_dollar_amount)
   end
 
-  def get_openers(first_element, true) do
-    ""
+  defp get_openers(_, true) do
+    nil
   end
 
-  def get_openers(first_element, false) do
-    first_element
+  defp get_openers(first_element_text, false) do
+    first_element_text
+  end
+
+  def parse_price_elements(price_elements) do
+    Enum.map(price_elements, &check_price_element(&1))
+    |> Enum.reject(&(nil == &1))
+    |> List.last
+  end
+
+  defp check_price_element(element) do
+    element_text = Floki.text(element) |> String.trim
+    is_dollar_amount = Regex.match?(~r/\$\d/, element_text)
+
+    get_price(element_text, is_dollar_amount)
+  end
+
+  defp get_price(_, false) do
+    nil
+  end
+
+  defp get_price(element_text, true) do
+    element_text
+  end
+
+  def parse_date_elements(date_elements) do
+    datetime = date_elements
+               |> Floki.text
+               |> String.trim
+               # |> String.trim <> " 2016"
+               # |> Timex.parse!("%A, %B %e @ %k:%M %P %Y", :strftime)
+               # |> Timex.format!("%FT%TZ", :strftime)
+               # |> Ecto.DateTime.cast!
   end
 end
